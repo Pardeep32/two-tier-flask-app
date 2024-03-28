a0f75dcf43dd89e23b699aee805490df924d14b9
pardeep kaur <137564227+Pardeep32@users.noreply.github.com>
2024-03-28 13:36:57 -0400

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-locking-bucket-hotstar" # Replace with your actual S3 bucket name
+    bucket = "redditbucketclone1708r" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ca-central-1"
   }
