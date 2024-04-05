cc9ca6e9e163aec98eec698434d05eb5c5c40468
pardeep kaur <137564227+Pardeep32@users.noreply.github.com>
2024-04-05 12:09:53 -0400

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "redditbucketclone1708" # Replace with your actual S3 bucket name
+    bucket = "2tier-project-flask" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ca-central-1"
   }
